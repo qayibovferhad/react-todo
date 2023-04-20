344df8fa557d8238ba19b9d5c2bd679f56c06ae9
Ferhad <qayibovferhad04@gmail.com>
2023-04-21 01:58:49 +0400

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -32,52 +32,5 @@ function App() {
     </div>
   );
 }
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       todos: [],
-//     };
-//     this.handleFormSubmit = this.handleFormSubmit.bind(this);
-//     this.handleDelete = this.handleDelete.bind(this);
-//   }
-//   handleFormSubmit(todoInput) {
-//     this.setState(
-//       (prevstate) => {
-//         return {
-//           todos: [...prevstate.todos, todoInput.value],
-//         };
-//       },
-//       () => {
-//         todoInput.value = "";
-//       }
-//     );
-//   }
-//   handleDelete(index) {
-//     this.setState((prevstate) => {
-//       const oldTodos = prevstate.todos.slice();
-//       oldTodos.splice(index, 1);
-//       return {
-//         todos: oldTodos,
-//       };
-//     });
-//   }
-//   render() {
-//     const { todos } = this.state;
-//     return (
-//       <div className="w-1/2 mx-auto mt-5">
-//         <h1 className="text-lg font-bold text-center my-2">Todo App</h1>
-//         <TodoForm onSubmit={this.handleFormSubmit} />
-//         <TodoList todos={todos} handleDelete={this.handleDelete} />
-//         <MouseTracker
-//           render={({ x, y }) => (
-//             <h1 className="tracking-wide text-xl font-bold text-center">
-//               X is {x} and Y is {y}
-//             </h1>
-//           )}
-//         />
-//       </div>
-//     );
-//   }
-// }
+
 export default App;
